9481f3a28ec7cb62803261e3959ecc9be66cd613
rounak161 <142321950+rounak161@users.noreply.github.com>
2024-07-12 21:05:13 +0530

    code update

--- backend/index.js
+++ backend/index.js
@@ -38,8 +38,7 @@ app.listen(port, () => console.log("Server has been started"));
  
 //  // Root route
 // app.get('/', (req, res) => {
-//   res.send('Welcome to the Travello backend service!');
-// });
+ 
 
  
  
